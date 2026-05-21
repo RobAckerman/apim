cdabff9700826c33cef45c9a73ab0d3ee4bbf735
RobAckerman <129887996+RobAckerman@users.noreply.github.com>
2026-05-21 12:12:02 -0500

    Update utils.R

--- R/utils.R
+++ R/utils.R
@@ -1,9 +1,2 @@
-#' Null coalescing operator
-#'
-#' Returns \code{a} if it is not \code{NULL}, otherwise returns \code{b}.
-#'
-#' @param a Left-hand side value.
-#' @param b Right-hand side default value.
-#' @return \code{a} if not \code{NULL}, else \code{b}.
-#' @keywords internal
+# internal null coalescing operator
 `%||%` <- function(a, b) if(!is.null(a)) a else b
